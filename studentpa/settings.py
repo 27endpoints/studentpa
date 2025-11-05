4a04acdb46ef9e7b45cdfb587c1d4094968f759e
27endpoints <lethuh527@gmail.com>
2025-11-05 12:26:17 +0200

    debug true

--- studentpa/settings.py
+++ studentpa/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECURITY_KEY')
+DEBUG=True
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+# DEBUG = os.getenv('DEBUG') == 'True'
 ALLOWED_HOSTS = ['studentpa.onrender.com',
                   'www.studentpa.online', 
                   'studentpa.online']
