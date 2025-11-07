e0866514c6555821d0bc4795ea51f3da961c2795
27endpoints <lethuh527@gmail.com>
2025-11-07 03:37:45 +0200

    removed the slugs

--- seo/models.py
+++ seo/models.py
@@ -1,17 +1,11 @@
 from django.db import models
-from django.utils.text import slugify
 
 class Region(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         verbose_name = "Main Region / Town"
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -20,16 +14,10 @@ class Region(models.Model):
 class Subregion(models.Model):
     region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="subregions")
     name = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         verbose_name = "Area / Neighborhood"
-        unique_together = ('region', 'name')
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super().save(*args, **kwargs)
+       
 
     def __str__(self):
         return f"{self.name} ({self.region.name})"
